94c529cd602cd409026a2bfd362b85f424bdf7fc
freeintelligence <free04381@gmail.com>
2020-02-29 03:23:11 -0300

    Se agregan los recursos que faltaban por exportar.

--- projects/simple-forms/src/public-api.ts
+++ projects/simple-forms/src/public-api.ts
@@ -11,6 +11,8 @@ export * from './lib/components/button/button.component';
 
 export * from './lib/services/simple-forms.service';
 
+export * from './lib/interfaces/button.interface';
 export * from './lib/interfaces/custom-header.interface';
 export * from './lib/interfaces/dialog-custom-arguments.interface';
 export * from './lib/interfaces/field.interface';
+export * from './lib/interfaces/submit.interface';
