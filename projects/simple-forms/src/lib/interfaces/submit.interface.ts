f5669f1ea4d720f524231bc9cf52cb7cdc062211
freeintelligence <free04381@gmail.com>
2020-02-29 02:38:34 -0300

    Mejoras..

--- projects/simple-forms/src/lib/interfaces/submit.interface.ts
+++ projects/simple-forms/src/lib/interfaces/submit.interface.ts
@@ -3,7 +3,13 @@ import { Button } from './button.interface';
 export interface Submit {
   url: string;
   method: 'post'|'POST'|'get'|'GET'|'patch'|'PATCH'|'put'|'PUT'|'delete'|'DELETE';
-  errorMessage?: string;
-  successMessage?: string;
-  buttons: Button[];
+  error?: {
+    message?: string;
+    handle?: () => any;
+  },
+  success?: {
+    message?: string;
+    buttons?: Button[];
+    handle?: () => any;
+  }
 }
