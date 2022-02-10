27c382769fa2f5d4a69ddefbee62a014e4339af2
FreeIntelligence <free04381@gmail.com>
2022-02-10 18:46:53 -0300

    Pruebas para ver que falla..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SimpleFormsService } from 'ngx-simple-forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ngx-simple-forms-workspace';
+
+  constructor(private service: SimpleFormsService) {
+
+  }
+
+  ngOnInit(): void {
+    this.service.createDialogForm({
+      header: {
+        title: 'Formulario',
+      },
+      message: 'veamos si funciona el mensaje',
+      buttons: [
+        { text: 'Enviar', tooltip: '', color: 'primary', },
+      ],
+      fields: {
+        name: {
+          key: 'name',
+          placeholder: 'Ingresa el nombre',
+        }
+      }
+    })
+  }
+
 }
